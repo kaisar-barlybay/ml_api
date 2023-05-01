c47fcdb790708567fc7b3f4e0aca54c28ab500b0
kaucapbm94 <48528021+kaucapbm94@users.noreply.github.com>
2023-05-01 22:24:48 +0600

    v

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='api',
-    version='0.00.10',
+    version='0.00.11',
     author='Kaisar Barlybay',
     author_email='kaisar.barlybay.sse@gmail.com',
     description='Testing installation of Package',
